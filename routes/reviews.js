4b6bc52daa7ec601523de9f851f96687fe9c1006
coderMayank.69 <mayankbca96325@gmail.com>
2025-09-17 17:11:23 +0530

    deployed the mvc framework by adding controllers

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,28 +1,16 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const Review = require('../models/review');
+const reviews = require('../controllers/reviews');
 const Campground = require('../models/campground');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const {validateReview,isLoggedIn,isReviewAuthor} = require('../middleware.js');
+const { createReview } = require('../controllers/reviews.js');
 
 
-router.post('/',validateReview,isLoggedIn, catchAsync(async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!');
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-router.delete('/:reviewID',isLoggedIn,isReviewAuthor,catchAsync( async (req,res) =>{
-    const {id, reviewID} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewID}})
-    await Review.findByIdAndDelete(reviewID);
-    req.flash('success', 'Successfully deleted review!');
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.post('/',validateReview,isLoggedIn, catchAsync(reviews.createReview))
+
+router.delete('/:reviewID',isLoggedIn,isReviewAuthor,catchAsync(reviews.deleteReview))
 
 module.exports = router;
