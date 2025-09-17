bc138d5cf252efd6c1a5041fbc695bbc70a49053
coderMayank.69 <mayankbca96325@gmail.com>
2025-09-17 17:27:33 +0530

    make routes file clean by using router.route function

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,14 @@ const User = require('../models/user');
 const passport = require('passport');
 const users = require('../controllers/users');
 const { storeReturnTo } = require('../middleware');
-const user = require('../models/user');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-router.post('/register', catchAsync(users.register));
-
-
-router.get('/login',users.renderLogin);
-
-router.post('/login',storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
-
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
 router.get('/logout',users.logout); 
 
